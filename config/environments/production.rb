a3793d6001979d0d03c0bb5ed8d435572c4279d7
Dave Martin <justanotherdavemartin@gmail.com>
2015-01-15 17:04:13 +0000

    minor changes for heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,4 +64,12 @@ MembersDatabase::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
+
+    config.action_mailer.default_url_options = { :host => 'vast-waters-8717.herokuapp.com' }
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default :charset => "utf-8"
+
+  config.action_mailer.perform_deliveries = true
+
 end
